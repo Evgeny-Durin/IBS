2cfb838d42098916c40f218f14e7ac872ac1851b
CodeDealer <Ksionxd@gmail.com>
2021-09-07 22:51:47 +0300

    add class DataType

---
+++ src/DataType.java
@@ -0,0 +1,8 @@
+public class DataType {
+    public static void main(String[] args) {
+        double a = 3.1416;
+        byte b = 127;
+        System.out.println("double - переменная типа с плавающей точкой с наибольшим диапазоном чисел = " + a);
+        System.out.print("byte - переменная целочисленного типа с наименьшим диапазоном чисел = " + b);
+    }
+}
