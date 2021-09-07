f1d7d14be16edfd7ba4f6a0cfd68348b880d83ab
CodeDealer <Ksionxd@gmail.com>
2021-09-07 21:07:03 +0300

    add array

---
+++ src/Array.java
@@ -0,0 +1,29 @@
+public class Array {
+    public static int getRandomDiceNumber() {
+        return (int) (Math.random() * 6);
+    }
+    public static void main(String[] args) {
+        int[] myArray = new int[5];
+        System.out.println("Задание 1:");
+        for (int i = 0; i < myArray.length; i++) {
+            myArray[i] = getRandomDiceNumber();
+            System.out.print(" " + myArray[i] + " ");
+        }
+        System.out.println();
+        System.out.println("Задание 2:");
+        int x = myArray[0];
+        int y = myArray[myArray.length-1];
+        myArray[0] = y;
+        myArray[myArray.length-1] = x;
+        for (int j : myArray) {
+            System.out.print(" " + j + " ");
+        }
+        System.out.println();
+        System.out.println("Задание 3:");
+        int avg =  myArray[0] + myArray[2];
+        System.out.print("Сумма первого и среднего элементов = " + avg);
+    }
+}
+
+
+
